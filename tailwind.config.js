82257e389aaa3303d3173044213cb5504facd250
farlos <thiraphat.pant@gmail.com>
2024-11-13 20:59:17 +0700

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'prompt': ['Prompt', 'sans-serif'],
+      },
+      fontWeight: {
+        thin: 100,
+        extralight: 200,
+        light: 300,
+        regular: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
