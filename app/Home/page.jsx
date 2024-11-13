10f9af65e7800c2a48c1fd67a95f6b5cb2aa554d
Your Name <your-email@example.com>
2024-11-14 00:38:35 +0700

    hompage finish

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -27,38 +27,156 @@ export default function Home() {
           </div>
         </div>
         <div className="py-8">
-          <div className="container">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:gid-cols-4 gap-8">
+          <div className="container ml-[150px] ">
+            <div className="grid grid-cols-3 gap-[50px] border w-max ">
               {/* today weather */}
-              {/* <div className="py-10 p-5 bg-white rounded-[25px] relative h-[400px] flex items-start ">
+              <div className="p-5 bg-white rounded-[25px] h-[500px] w-[300px]">
                 <div>
-                  <div className="mb-4">
+                  <div className="p-4 flex flex-col space-y-4">
                     <p className="font-bold text-[42px]">วันนี้</p>
                     <p>อ.12</p>
                     <p>24°</p>
                     <p>มีหมอกน้ำค้าง</p>
-                    <span className="flex gap-4">☀️<div className="bg-[black] w-[2px]"></div><p>3</p></span>
-                    <span className="flex gap-4">🌬️<p>5 กม./ชม.</p></span>
-                    <span className="flex">💧<p>86%</p></span>
+                    <span className="flex gap-4">
+                      ☀️<div className="bg-[black] w-[2px]"></div>
+                      <p>3</p>
+                    </span>
+                    <span className="flex gap-4">
+                      🌬️<div className="bg-[black] w-[2px]"></div>
+                      <p>5 กม./ชม.</p>
+                    </span>
+                    <span className="flex gap-4">
+                      💧<div className="bg-[black] w-[2px]"></div>
+                      <p>86%</p>
+                    </span>
                     <p className="">
                       คำแนะนำ:
                       ควรดื่มน้ำอย่างเพียงพอเพื่อลดความเสี่ยงจากสภาพอากาศที่แห้ง
                     </p>
                   </div>
                 </div>
-              </div> */}
+              </div>
               {/* other's day weather */}
-              <div></div>
+              <div className="grid grid-cols-3 grid-rows-2 gap-5 col-span-2">
+                <div className="bg-white rounded-[25px] p-3">
+                  <p className="font-bold text-[42px]">วันนี้</p>
+                  <p>อ.12</p>
+                  <p>24°</p>
+                  <p>มีหมอกน้ำค้าง</p>
+                  <span className="flex gap-4">
+                    ☀️<div className="bg-[black] w-[2px]"></div>
+                    <p>3</p>
+                  </span>
+                  <span className="flex gap-4">
+                    🌬️<div className="bg-[black] w-[2px]"></div>
+                    <p>5 กม./ชม.</p>
+                  </span>
+                  <span className="flex gap-4">
+                    💧<div className="bg-[black] w-[2px]"></div>
+                    <p>86%</p>
+                  </span>
+                </div>
+                <div className="bg-white rounded-[25px] p-3">
+                  <p className="font-bold text-[42px]">วันนี้</p>
+                  <p>อ.12</p>
+                  <p>24°</p>
+                  <p>มีหมอกน้ำค้าง</p>
+                  <span className="flex gap-4">
+                    ☀️<div className="bg-[black] w-[2px]"></div>
+                    <p>3</p>
+                  </span>
+                  <span className="flex gap-4">
+                    🌬️<div className="bg-[black] w-[2px]"></div>
+                    <p>5 กม./ชม.</p>
+                  </span>
+                  <span className="flex gap-4">
+                    💧<div className="bg-[black] w-[2px]"></div>
+                    <p>86%</p>
+                  </span>
+                </div>
+                <div className="bg-white rounded-[25px] p-3">
+                  <p className="font-bold text-[42px]">วันนี้</p>
+                  <p>อ.12</p>
+                  <p>24°</p>
+                  <p>มีหมอกน้ำค้าง</p>
+                  <span className="flex gap-4">
+                    ☀️<div className="bg-[black] w-[2px]"></div>
+                    <p>3</p>
+                  </span>
+                  <span className="flex gap-4">
+                    🌬️<div className="bg-[black] w-[2px]"></div>
+                    <p>5 กม./ชม.</p>
+                  </span>
+                  <span className="flex gap-4">
+                    💧<div className="bg-[black] w-[2px]"></div>
+                    <p>86%</p>
+                  </span>
+                </div>
+                <div className="bg-white rounded-[25px] p-3">
+                  <p className="font-bold text-[42px]">วันนี้</p>
+                  <p>อ.12</p>
+                  <p>24°</p>
+                  <p>มีหมอกน้ำค้าง</p>
+                  <span className="flex gap-4">
+                    ☀️<div className="bg-[black] w-[2px]"></div>
+                    <p>3</p>
+                  </span>
+                  <span className="flex gap-4">
+                    🌬️<div className="bg-[black] w-[2px]"></div>
+                    <p>5 กม./ชม.</p>
+                  </span>
+                  <span className="flex gap-4">
+                    💧<div className="bg-[black] w-[2px]"></div>
+                    <p>86%</p>
+                  </span>
+                </div>
+                <div className="bg-white rounded-[25px] p-3">
+                  <p className="font-bold text-[42px]">วันนี้</p>
+                  <p>อ.12</p>
+                  <p>24°</p>
+                  <p>มีหมอกน้ำค้าง</p>
+                  <span className="flex gap-4">
+                    ☀️<div className="bg-[black] w-[2px]"></div>
+                    <p>3</p>
+                  </span>
+                  <span className="flex gap-4">
+                    🌬️<div className="bg-[black] w-[2px]"></div>
+                    <p>5 กม./ชม.</p>
+                  </span>
+                  <span className="flex gap-4">
+                    💧<div className="bg-[black] w-[2px]"></div>
+                    <p>86%</p>
+                  </span>
+                </div>
+                <div className="bg-white rounded-[25px] p-3">
+                  <p className="font-bold text-[42px]">วันนี้</p>
+                  <p>อ.12</p>
+                  <p>24°</p>
+                  <p>มีหมอกน้ำค้าง</p>
+                  <span className="flex gap-4">
+                    ☀️<div className="bg-[black] w-[2px]"></div>
+                    <p>3</p>
+                  </span>
+                  <span className="flex gap-4">
+                    🌬️<div className="bg-[black] w-[2px]"></div>
+                    <p>5 กม./ชม.</p>
+                  </span>
+                  <span className="flex gap-4">
+                    💧<div className="bg-[black] w-[2px]"></div>
+                    <p>86%</p>
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
           <div></div>
         </div>
 
         <div className="grid grid-row-2 ">
-          <h3 className="text-xl absolute top-7 right-2 font-bold">
+          <h3 className="text-xl absolute top-[60px] right-2 font-bold">
             ประเทศไทย
           </h3>
-          <div className=" absolute  top-20 right-2 w-[500px] h-full max-h-[700px] border"></div>
+          <div className="absolute top-[100px] right-2 w-[500px] h-full max-h-[700px] border p-4"></div>
         </div>
         <div></div>
         <footer className="mt-auto">
