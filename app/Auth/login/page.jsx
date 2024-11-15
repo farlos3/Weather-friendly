41c1ccee04374b0673301ad9df6dbce370d589e1
farlos <thiraphat.pant@gmail.com>
2024-11-16 06:27:48 +0700

    Login and Signup redirect, Without JWT Authentication

--- app/Auth/login/page.jsx
+++ app/Auth/login/page.jsx
@@ -47,10 +47,10 @@ export default function RegisterPage() {
       });
 
       if (res.ok) {
-        const data = await res.json();
+        // const data = await res.json();
         setError('');
         setSuccess("Login successful!");
-        console.log(data);
+        // console.log(data);
         setTimeout(() => {
           router.push(redirectTo);
         }, 500);
@@ -65,26 +65,49 @@ export default function RegisterPage() {
       console.log("Error during login: ", error);
     }
   }
+
+  // ฟังก์ชันสำหรับล็อกอินผ่าน Google
+  async function handleGoogleLogin() {
+    try {
+      const res = await fetch("http://localhost:3000/backend/api/auth/google", {
+        method: "GET",
+        credentials: "include", // ใช้เพื่อจัดการ cookie
+      });
+      
+      if (res.ok) {
+        setSuccess("Login with Google successful!");
+        setTimeout(() => {
+          router.push(redirectTo);
+        }, 500);
+      } else {
+        setError("Google login failed.");
+      }
+    } catch (error) {
+      setError("Error during Google login. Please try again.");
+    }
+  }
   
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="bg-white p-8 rounded shadow-md w-80">
         <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
         <form onSubmit={handleSubmit}>
-
           {error && (
-              <div className="bg-red-500 border border-red-600 rounded-lg px-3 p-2 flex items-center space-x-3 text-white">{error}</div>
-            )}
-
+            <div className="bg-red-500 border border-red-600 rounded-lg px-3 py-2 mb-4 text-white">
+              {error}
+            </div>
+          )}
           {success && (
-              <div className="bg-green-500 border border-green-600 rounded-lg px-3 p-2 flex items-center space-x-3 text-white">{success}</div>
-            )}
-            
+            <div className="bg-green-500 border border-green-600 rounded-lg px-3 py-2 mb-4 text-white">
+              {success}
+            </div>
+          )}
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
               Email
             </label>
-            <input onChange={(event => setEmail(event.target.value))}
+            <input
+              onChange={(event) => setEmail(event.target.value)}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="email"
               id="email"
@@ -96,28 +119,38 @@ export default function RegisterPage() {
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
               Password
             </label>
-            <input onChange={(event => setPassword(event.target.value))}
+            <input
+              onChange={(event) => setPassword(event.target.value)}
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="password"
               id="password"
               placeholder="Enter password"
             />
           </div>
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
-          >
-            Login
-          </button>
+          <div className="space-y-4">
+            <button
+              onClick={handleGoogleLogin}
+              type="submit"
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full"
+            >
+              Login with Google
+            </button>
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            >
+              Login
+            </button>
+          </div>
+          <div className="mt-4 text-center">
+            <p>
+              ไม่มีบัญชีผู้ใช้?{" "}
+              <Link href="./signup" legacyBehavior>
+                <a className="text-blue-500 hover:text-blue-700">ลงทะเบียนที่นี่</a>
+              </Link>
+            </p>
+          </div>
         </form>
-        <div className="mt-4 text-center">
-          <p>
-            ไม่มีบัญชีผู้ใช้?{" "}
-            <Link href="./signup" legacyBehavior>
-              <a className="text-blue-500 hover:text-blue-700">ลงทะเบียนที่นี่</a>
-            </Link>
-          </p>
-        </div>
       </div>
     </div>
   );
--- app/backend/api/auth/login/route.js
+++ app/backend/api/auth/login/route.js
@@ -6,7 +6,6 @@ import bcryptjs from 'bcryptjs';
 export async function POST(request) {
     try {
         const { email, password } = await request.json();
-
         await connectMongoDB();
 
         const user = await User.findOne({ email });
@@ -16,13 +15,10 @@ export async function POST(request) {
             return NextResponse.json({ message: "Invalid email or password." }, { status: 401 });
         }
 
-        // คุณอาจต้องการสร้าง JWT token หรือ session ที่นี่เพื่อนำไปใช้ใน authentication
-        // const secret = process.env.JWT_SECRET
-
         console.log("Login successful!✅")
 
         return NextResponse.json({ message: "Login successful." }, { status: 200 });
-        
+
     } catch (error) {
         return NextResponse.json({ message: "An error occurred during login. Please try again." }, { status: 500 });
     }
